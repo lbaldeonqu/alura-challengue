a2d9a36a30ccc935ef38afaf7c4e1e45cd6b02cb
Luis Benjamin Baldeon Quispe <lbaldeonqu@proveedor.intercorp.com.pe>
2024-02-08 16:10:48 -0500

    endproject

---
+++ script.js
@@ -0,0 +1,71 @@
+let palabra = "";
+let palabraEncriptada = "";
+
+function encriptar() {
+  palabra = document.getElementById("txtarearequest").value.toLowerCase();
+  const conversiones = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+  const palabraEncriptar = palabra
+    .split("")
+    .map((letra) => conversiones[letra] || letra)
+    .join("");
+  return palabraEncriptar;
+}
+
+function mostrarEncriptar() {
+  let inputResultado = document.getElementById("txtarearesponse");
+  inputResultado.style.backgroundImage = "none";
+  inputResultado.innerHTML = encriptar();
+  let btncopiar = document.getElementById("btncopiar");
+  btncopiar.style.display = "block";
+  let inputrequestLimpiar = document.getElementById("txtarearequest");
+  inputrequestLimpiar.value="";
+}
+
+function desencriptar() {
+  let campoEncriptado = document.getElementById("txtarearequest").value;
+  const conversionesInversas = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
+  const palabraOriginal = campoEncriptado.split(/(enter|imes|ai|ober|ufat)/).map((letra) => conversionesInversas[letra] || letra).join("");
+  return palabraOriginal;
+}
+
+function mostrarDesencriptar() {
+  let inputResultado2 = document.getElementById("txtarearesponse");
+  inputResultado2.style.backgroundImage = "none";
+  inputResultado2.value = desencriptar();
+  btncopiar.style.display = "block";
+  let inputrequestLimpiar = document.getElementById("txtarearequest");
+  inputrequestLimpiar.value="";
+}
+
+function btncopy() {
+  let campoEncriptado = document.getElementById("txtarearesponse");
+
+  campoEncriptado.select();
+  campoEncriptado.setSelectionRange(0,campoEncriptado.value.length); /* Para dispositivos móviles */
+
+  /* Intenta copiar el texto al portapapeles */
+  navigator.clipboard
+    .writeText(campoEncriptado.value)
+    .then(function () {
+      /* Éxito: el texto se ha copiado correctamente */
+      alert("Texto copiado al portapapeles: " + campoEncriptado.value);
+      location.reload();
+    })
+    .catch(function (err) {
+      /* Manejar errores en caso de que la copia al portapapeles falle */
+      console.error("Error al copiar al portapapeles: ", err);
+    });
+}
+
